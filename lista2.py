14f4095d73b4fed77f46dd01d74990538088e706
SoaresDavidson <davi504r35@gmail.com>
2024-09-28 09:12:45 -0300

    exclude do saleh agora é 6 linhas e limpa o terminal

--- lista2.py
+++ lista2.py
@@ -1,3 +1,7 @@
+from os import system,name
+#limpa o terminal
+def clear():
+    system("cls" if name == "nt" else "clear");
 class Node():
     def __init__(self, elem):
         self.elem = elem
@@ -15,38 +19,39 @@ class Node():
     
 class DoubleList():
     def __init__(self):
-        self.head = Node(None)
-        self.tail = Node(None)
+        self.head = None
+        self.tail = None
         self.count = 0
 
     def push_back(self, elem): #* Adiciona o elemento ao final da lista
-        if self.tail.elem == None:
-            self.tail.elem = elem
+        if self.tail == None:
+            self.tail = Node(elem)
             self.tail.prev = self.head
             self.tail.next = None
-        else:
-            n = Node(elem)
-            self.tail.next = n
-            n.prev = self.tail
-            self.tail = n
-        self.count += 1
+            return
+        
+        n = Node(elem)
+        self.tail.next = n
+        n.prev = self.tail
+        self.tail = n
 
     def push_front(self, elem): #*Adiciona o elemento ao começo da lista
-        if self.head.elem == None:
-            self.head.elem = elem
+        if self.head == None:
+            self.head = Node(elem)
             self.head.prev = None
             self.head.next = self.tail
-            self.tail.prev = self.head
-        else:
-            n = Node(elem)
-            self.head.prev = n
-            if self.head.next is self.tail and self.tail.elem is None:
-                self.tail = self.head
-                self.tail.next = None
-            n.next = self.head
-            n.prev = None
-            self.head = n
-        self.count += 1
+            self.tail = self.head
+            return
+    
+        n = Node(elem)
+        self.head.prev = n
+        if self.head.next is self.tail and self.tail.elem is None:
+            self.tail = self.head
+            self.tail.next = None
+        n.next = self.head
+        n.prev = None
+        self.head = n
+
     
     def getTail(self):
         return self.tail
@@ -55,52 +60,45 @@ class DoubleList():
         return self.head
     
     def insert(self, elem): #* Função de inserção ordenada em ordem crescente
-        if self.head.elem == None or elem < self.head.elem:
+        self.count += 1
+        if self.head == None or elem <= self.head.elem:
             self.push_front(elem)
-        elif self.tail.elem == None or elem > self.tail.elem:
+            return
+            
+        if self.tail == None or elem >= self.tail.elem:
             self.push_back(elem)
-        else: #* Insere o elemento em seu devido lugar, respeitando a ordem crescente
-            n = self.head
-            while n is not None:
-                if n.elem > elem:
-                    break
-                n = n.next
-            new = Node(elem)
-            n.prev.next = new
-            n.prev = new
-            new.prev = n.prev
-            new.next = n
+            return
+        
+        #* Insere o elemento em seu devido lugar, respeitando a ordem crescente
+        n = self.head
+        while n is not None:
+            if n.elem > elem:
+                break
+            n = n.next
+        new = Node(elem)
+
+        n.prev.next = new
+        new.prev = n.prev
+        n.prev = new
+        new.next = n
+       
 
     def isThere(self, elem):   #* Verifica se um elemento está na lista
         n = self.head
         while n != None:
             if n.elem == elem:
-                return True
+                return n
             n = n.next
-        return False
+        return None
     
     def exclude(self, elem):
-        if self.isThere(elem): #*Checa se o elemento a ser excluído existe
-            self.count -= 1
-            n = self.head
-            while n.elem is not None:
-                if n.elem == elem:
-                    break
-                n = n.next
-            if n == self.head: #*Caso o elemento seja o head, a gente precisa alterá-lo diretamente
-                self.head = n.next
-            elif n == self.tail: #*O mesmo serve para a tail
-                self.tail = n.prev
-            previous = n.prev
-            next = n.next
-            #*Nódulos nulos não podem ter ponteiros não nulos
-            if previous is not None:
-                previous.next = next
-            if next is not None:
-                next.prev = previous
-            return True
-        else:
-            return False
+        oldElem = self.isThere(elem)
+        if oldElem == None: return False
+        if oldElem.prev != None: oldElem.prev.next = oldElem.next
+        if oldElem.next != None: oldElem.next.prev = oldElem.prev
+        if self.head == oldElem: self.head = oldElem.next
+        self.count -= 1
+        return True
     
     def getCount(self):
         return int(self.count)
@@ -109,8 +107,7 @@ class DoubleList():
         elements = []
         n = self.head
         while n != None:
-            if n.elem != None:
-                elements.append(str(n))
+            elements.append(str(n))
             n = n.next
         return elements
             
@@ -118,8 +115,10 @@ class DoubleList():
 print("Bem vindo ao simulador de lista encadeada dupla. Segue menu de opções")
 lista = DoubleList()
 while True:
+    
     print("1 - Adicionar elementos\n2 - Verificar se elemento está na lista\n3 - Excluir elemento\n4 - Exibir todos os elementos e quantidade\nOutro - Fechar o programa")
     x = input()
+    clear()
     try:
         x = int(x)
     except:
@@ -146,4 +145,5 @@ while True:
     else:
         print("Programa será fechado")
         exit()
+    
 
