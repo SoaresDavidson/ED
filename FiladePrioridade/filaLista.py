5b082bc651d5375c0d340ce9b708787bf770ab1c
victor-kauan-coder <victor.kauan@ufpi.edu.br>
2024-10-04 17:46:09 -0300

    atribui pesos aos objetos pra ficar mais facil de manipular, e criei mais algumas funções bases

--- FiladePrioridade/filaLista.py
+++ FiladePrioridade/filaLista.py
@@ -1,19 +1,22 @@
 class Pessoa:
+    peso = 0
     def __init__(self,nome,idade):
         self.next = None
         self.nome = nome
         self.idade = idade
+
+        
 
 class Priority_Queue:
     def __init__(self):
         self.head = None
         self.back = None
         self.filaComum = 0
+        self.atendidasComum = 0
+        self.atendidasPrioridade = 0
         self.filaPrioridade = 0
 
     def prioridade(self,pessoa):
-        if pessoa.idade < 60:
-            return 0
         if pessoa.idade < 70:
             return 1
         if pessoa.idade < 80:
@@ -22,18 +25,38 @@ class Priority_Queue:
             return 3
         if pessoa.idade < 100:
             return 4
-        if pessoa.idade >=100:
-            return 5                    
+        return 5                    
+
+    def size_prioridade(self):
+        return self.filaPrioridade - self.atendidasPrioridade
+
+    def size_comum(self):
+        return self.filaComum - self.atendidasComum    
+
 
     def push(self,nome,idade):
         pessoa = Pessoa(nome,idade)
         if idade >= 60:
             self.adicionar_prioridade(pessoa)
         else:
-            self.adicionar_comum(pessoa)  
+            self.adicionar_comum(pessoa) 
+    def empty(self):
+        if self.head is not None:
+            return False
+        return True
+
+    def pop_front(self):
+        if not self.empty():
+            aux = f'{self.head.nome},{self.head.idade}'
+            if self.head.peso != 0: #se a pessoa que esta no começo da fila for preferencial
+                self.atendidasPrioridade+=1
+            else:
+                self.atendidasComum+=1    
+            self.head = self.head.next 
+            return aux        
 
     def adicionar_prioridade(self,pessoa):
-        nível_prioridade = self.prioridade(pessoa)
+        pessoa.peso = self.prioridade(pessoa)
         self.filaPrioridade+=1
         if self.head is None:
             self.back = pessoa
@@ -42,7 +65,7 @@ class Priority_Queue:
         atual = self.head
         anterior = None 
         while atual is not None:
-            if  nível_prioridade > self.prioridade(atual):
+            if  pessoa.peso > atual.peso:
                 break
             anterior = atual
             atual = atual.next
@@ -72,8 +95,34 @@ class Priority_Queue:
             atual = atual.next   
         return msg
 
+def color(txt,cor):
+    print(f'\033[{cor}{txt}\033[m')
 
+def menu():
+    #alguem cria o menu e utiliza a função de cima ela modifica as cores no terminal
+    #se tiver duvida na internet tem os codigos das cores ou eu posso ensinar como fazer
+    pass    
+
+
+#teste
 fila = Priority_Queue()
-fila.push('ana',50)
+fila.push('ana',71)
 fila.push('joao',60)
-print(fila)              
+fila.push('pedro',50)
+fila.push('raquel',80)
+fila.push('maria',61)
+fila.push('saleh',19)
+fila.push('marcelo',63)
+fila.push('victor',62)
+fila.pop_front()
+fila.pop_front()
+print(fila.size_prioridade())
+print(fila.size_comum())
+print(fila)  
+
+#a parte principal vai ser aqui mas vou deixar comentado
+'''
+if __name__ == '__main__':
+    while True:
+        menu()
+'''
