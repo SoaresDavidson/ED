2f7e0fe7af8fefb8714408de6c99bf509d268347
victor-kauan-coder <victor.kauan@ufpi.edu.br>
2024-10-04 11:38:15 -0300

    cada intervalo tem um nivel de prioridade

---
+++ FiladePrioridade/filaLista.py
@@ -0,0 +1,65 @@
+class Pessoa:
+    def __init__(self,nome,idade):
+        self.next = None
+        self.nome = nome
+        self.idade = idade
+
+class Priority_Queue:
+    def __init__(self):
+        self.head = None
+        self.back = None
+        self.filaComum = 0
+        self.filaPrioridade = 0
+
+    def prioridade(self,pessoa):
+        if pessoa.idade < 70:
+            return 1
+        if pessoa.idade < 80:
+            return 2
+        if pessoa.idade < 90:
+            return 3
+        if pessoa.idade < 100:
+            return 4
+        if pessoa.idade >=100:
+            return 5   
+        else:
+            return 0                 
+
+    def push(self,nome,idade):
+        pessoa = Pessoa(nome,idade)
+        if idade >= 60:
+            self.adicionar_prioridade(pessoa)
+        else:
+            self.adicionar_comum(pessoa)  
+
+    def adicionar_prioridade(self,pessoa):
+        nível_prioridade = self.prioridade(pessoa)
+        self.filaPrioridade+=1
+        if self.head is None:
+            self.back = pessoa
+            self.head = pessoa
+            return 
+        atual = self.head
+        anterior = None 
+        while atual is not None:
+            if  nível_prioridade > self.prioridade(atual):
+                break
+            anterior = atual
+            atual = atual.next
+        if anterior is None:
+            pessoa.next = self.head
+            self.head = pessoa   
+        else:
+            pessoa.next = atual   
+            anterior.next = pessoa 
+        if pessoa.next is None:
+            self.back = pessoa    
+            
+    def adicionar_comum(self,pessoa):
+        self.filaComum+=1
+        if self.back is None:
+            self.head = pessoa
+            self.back = pessoa
+        else:
+            self.back.next = pessoa
+            pessoa = self.back            
