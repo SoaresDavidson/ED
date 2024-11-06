a6c1ffdfa753dfdc56c4d1a2b6786b8838aad61c
SoaresDavidson <davi504r35@gmail.com>
2024-11-06 18:45:03 -0300

    botei gitignore

--- AlgoritmosOrdenação/sortLogaritmos.py
+++ AlgoritmosOrdenação/sortLogaritmos.py
@@ -6,15 +6,15 @@ nomes250k = "AlgoritmosOrdenação/dados-20241102T131234Z-001/dados/nomes250k.tx
 nomes500k = "AlgoritmosOrdenação/dados-20241102T131234Z-001/dados/nomes500k.txt"
 nomes1000k = "AlgoritmosOrdenação/dados-20241102T131234Z-001/dados/nomes1000k.txt"
 
-with open(nomes50k) as lista:
+with open(nomes1000k) as lista:
     arr_quick = lista.readlines()
     arr_merge = arr_quick.copy()
     arr_heap = arr_quick.copy()
 
     tempo_merge = merge_sort(arr_merge)
-    tempo_quick = quick(arr_quick)
-    tempo_heap = heapSort(arr_heap)
+    #tempo_quick = quick(arr_quick)
+    #tempo_heap = heapSort(arr_heap)
     
     print(f"merge levou {tempo_merge} segundos")
-    print(f"heap levou {tempo_heap} segundos")
-    print(f"quick levou {tempo_quick} segundos")
+    #print(f"heap levou {tempo_heap} segundos")
+    #print(f"quick levou {tempo_quick} segundos")
