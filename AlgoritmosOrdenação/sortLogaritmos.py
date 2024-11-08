831b861f25b371b7fc216f3bc190ed4344373b68
SoaresDavidson <davi504r35@gmail.com>
2024-11-08 18:05:48 -0300

    resolvi o quick

--- AlgoritmosOrdenação/sortLogaritmos.py
+++ AlgoritmosOrdenação/sortLogaritmos.py
@@ -1,5 +1,7 @@
 from algoritmos import quickSort, merge_sort, heapSort, shellSort
 import menu
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 nomes50k = "AlgoritmosOrdenação/dados-20241102T131234Z-001/dados/nomes50k.txt"
@@ -7,8 +9,3 @@ nomes100k = "AlgoritmosOrdenação/dados-20241102T131234Z-001/dados/nomes100k.tx
 nomes250k = "AlgoritmosOrdenação/dados-20241102T131234Z-001/dados/nomes250k.txt"
 nomes500k = "AlgoritmosOrdenação/dados-20241102T131234Z-001/dados/nomes500k.txt"
 nomes1000k = "AlgoritmosOrdenação/dados-20241102T131234Z-001/dados/nomes1000k.txt"
-
-
-media = menu.calcula_media(nomes50k, heapSort)
-
-print(f"a media é {media:.2f}")
