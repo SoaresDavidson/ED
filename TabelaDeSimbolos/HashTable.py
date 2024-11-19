0e4e03f583d48c173f9033c9c288b2e39acf4c2f
SoaresDavidson <davi504r35@gmail.com>
2024-11-19 15:42:23 -0300

    hashtable conta por array

--- TabelaDeSimbolos/HashTable.py
+++ TabelaDeSimbolos/HashTable.py
@@ -36,6 +36,14 @@ class tabelaHash:
             current = current.next
         return None
     
+    def countByCapacity(self):
+        listaSum = [0 for _ in range(self.M)] 
+        for i,v in enumerate(self.table):
+            aux = v
+            while aux is not None:
+                listaSum[i] += 1
+                aux = aux.next
+        return listaSum
                 
 
 
